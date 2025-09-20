073a7ef1b1ae303215dc51950d520edcc58ce5fb
dvidEast <daviddlim11@gmail.com>
2025-09-20 14:13:52 -0700

    navbar design changes

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -6,10 +6,17 @@ export const Navbar = () => {
             <img
                 className={styles.logo}
                 alt="SUS Logo"
-                src="/white-logo.png"
+                src="/long-logo.png"
             />
             
-            <a href="https://www.susubc.ca/" className={styles.backlinkButton}>SUS Home Page</a>
+            <div className={styles.linkContainer}>
+                <a href="/letterman">Jacket</a>
+                <a href="/quarterZips">Quarter Zips</a>
+                <a href="/hoodies">Hoodies</a>
+                <a href="/crewnecks">Crewnecks</a>
+                <a href="/shorts">Shorts</a>
+                <a href="https://www.susubc.ca/" className={styles.backlinkButton}>SUS Home Page</a>
+            </div>
         </nav>
     )
 }
